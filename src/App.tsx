6c8042a29459c0bf71f20b1f4ffca51d102bde21
Ben Orenstein <ben.orenstein@gmail.com>
2025-04-18 11:29:30 -0400

    add redirect for camp mgr

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import SocialPreview from "./pages/SocialPreview";
 import KickoffEvent from "./pages/KickoffEvent";
+import CampaignManager from "./pages/CampaignManager";
 import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
@@ -22,6 +23,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/social-preview" element={<SocialPreview />} />
           <Route path="/kickoff" element={<KickoffEvent />} />
+          <Route path="/cm" element={<CampaignManager />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/CampaignManager.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+const CampaignManager = () => {
+  useEffect(() => {
+    window.location.href = 'https://docs.google.com/document/d/1vkdkECrM_6wDUVa51UMEmPMInbAXNjF5EfgJxFoxja4';
+  }, []);
+
+  return null;
+};
+
+export default CampaignManager;
